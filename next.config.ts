5788ca936ccbbc0994062c2d93096b566b169dfc
Nurdaulet Kubaidolla <nurdauletkubaidolla@MacBook-Air-Nurdaulet.local>
2026-06-28 01:44:02 +0500

    fix next config

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,4 @@ const nextConfig: NextConfig = {
   serverExternalPackages: ["bcryptjs"],
 };
 
-// Отключаем проверку SSL для Node.js (как мы делали с npm)
-process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
-
 export default nextConfig;
